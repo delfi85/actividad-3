ec70f7eee291321e169cffa0ebfa7fcf7df5b372
Delfina Glavas <89335@sistemas.frc.utn.edu.ar>
2023-09-19 20:17:43 -0300

    desafio

--- script.js
+++ script.js
@@ -38,3 +38,33 @@ function(event) {
 // Agregar evento de clic al botón "Agregar"
 document.getElementById("agregar").addEventListener("click",
 agregarTarea);
+
+// Función para mostrar tareas completadas
+function mostrarCompletadas() {
+    const tareas = document.querySelectorAll("li");
+    tareas.forEach(tarea => {
+        if (tarea.classList.contains("completed")) {
+            tarea.style.display = "flex";
+        } else {
+            tarea.style.display = "none";
+        }
+    });
+}
+    
+// Función para mostrar tareas pendientes
+function mostrarPendientes() {
+    const tareas = document.querySelectorAll("li");
+    tareas.forEach(tarea => {
+        if (!tarea.classList.contains("completed")) {
+            tarea.style.display = "flex";
+        } else {
+            tarea.style.display = "none";
+        }
+    });
+}
+
+// Agregar evento de clic al botón "Mostrar Completadas"
+document.getElementById("mostrarCompletadas").addEventListener("click", mostrarCompletadas);
+    
+// Agregar evento de clic al botón "Mostrar Pendientes"
+document.getElementById("mostrarPendientes").addEventListener("click", mostrarPendientes);
